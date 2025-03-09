338aac541e5a3432e8e7425f59688b17b168e0d2
navid bahrami <navidbahrami1996@gmail.com>
2025-03-09 23:48:32 +0330

    config graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi'
+import { DatabaseModule } from './common/database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal : true ,
     validationSchema : Joi.object({
       MONGODB_URI : Joi.string().required(),
     }),
-  })],
+  }),
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver : ApolloDriver ,
+    autoSchemaFile : true ,  
+  }) ,
+  DatabaseModule,
+  UsersModule ,
+],
   controllers: [AppController],
   providers: [AppService],
 })
