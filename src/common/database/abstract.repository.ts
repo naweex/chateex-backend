a58f0d9ed54d227db26c074050aae681f11ed6ba
navid bahrami <navidbahrami1996@gmail.com>
2025-03-10 18:35:53 +0330

    abstract repository

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,9 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+import { Model } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+}
