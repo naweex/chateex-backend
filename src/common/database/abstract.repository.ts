3407c17f194b5845aac58ae156b08715439316b7
navid bahrami <navidbahrami1996@gmail.com>
2025-03-10 18:37:33 +0330

    abstract repository

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -1,4 +1,4 @@
-import { Logger, NotFoundException } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
 import { Model } from 'mongoose';
 
