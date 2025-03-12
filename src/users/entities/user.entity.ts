a10f3e1a709bb961aea43b5b528fa7bfe9bedbb8
navid bahrami <navidbahrami1996@gmail.com>
2025-03-12 20:51:54 +0330

    add Field,objectType

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,12 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { AbstractEntity } from 'src/common/database/abstract.entity';
 
 @Schema({ versionKey: false })
+@ObjectType()
 export class User extends AbstractEntity {
   @Prop()
+  @Field()//just allowed to email to have Field,dont use for password for more security.
   email: string;
   @Prop()
   password: string;
