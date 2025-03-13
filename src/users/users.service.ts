f01d1361f763c1a46cebde04084ce1a47fb311d9
navid bahrami <navidbahrami1996@gmail.com>
2025-03-13 23:11:39 +0330

    add create user

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository : UserRepository){}
+  async create(createUserInput: CreateUserInput) {
+    return this.userRepository.create({
+      ...createUserInput ,
+      password : await bcrypt.hash(createUserInput.password , 10)//10 for salt
+    });
+  }
+
+  async findAll() {
+    return this.userRepository.find({});
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
